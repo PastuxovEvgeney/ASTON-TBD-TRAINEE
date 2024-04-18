21a4ec4c58662552dd9f20365703858fa8c7d3fe
PastuxovEvgeney <161503998+PastuxovEvgeney@users.noreply.github.com>
2024-04-18 21:02:53 +0300

    Update Task1.java
    refactoring

--- Pastuhov_Java/src/main/java/org/example/Task1.java
+++ Pastuhov_Java/src/main/java/org/example/Task1.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Task1 {
     public static void main(String[] args) {
         Reader inputStreamReader = new InputStreamReader(System.in);
